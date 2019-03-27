9b15ff8836d1e7e1721c5fa038d2e1ef91ef3470
Everton Antunes de Oliveira <everton.oliveira@pixeon.com>
2019-03-27 12:05:59 -0300

    start project

---
+++ projects/intro/source/main.cpp
@@ -0,0 +1,67 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <iostream>
+
+const int WIDTH = 1280;
+const int HEIGHT = 960;
+
+void OnInit()
+{
+    glClearColor(1,0,0,0);
+    std::cout << "Initialization successfull" << std::endl;
+}
+
+void OnShutdown()
+{
+    std::cout << "Shutdown successfull" << std::endl;
+}
+
+void OnResize(int , int )
+{
+
+}
+
+void OnRender()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+int main( int argc, char **argv )
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitContextVersion (3, 0);
+    glutInitContextFlags (GLUT_CORE_PROFILE | GLUT_DEBUG);
+    glutInitContextProfile(GLUT_FORWARD_COMPATIBLE);
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutCreateWindow("Getting started with OpenGL 3.0");
+
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+    }
+    else
+    {
+        if (GLEW_VERSION_3_0)
+        {
+            std::cout << "Driver supports OpenGL 3.0\nDetails:" << std::endl;
+        }
+    }
+
+    std::cout << "\tUsing glew " << glewGetString(GLEW_VERSION) << std::endl;
+    std::cout << "\tVendor: "    << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "\tRenderer: "  << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "\tVersion: "   << glGetString(GL_VERSION) << std::endl;
+    std::cout << "\tGLSL: "      << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+
+    OnInit();
+    glutCloseFunc(OnShutdown);
+    glutDisplayFunc(OnRender);
+    glutReshapeFunc(OnResize);
+    glutMainLoop();
+
+    return 0;
+}
